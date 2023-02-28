a8722feb8da75983dad6e2451a1260f4710ac891
Luis Bovo <luis@fillet.com.br>
2023-02-28 12:22:12 -0300

    Delay inserido no scroll de slides do carrossel

--- components/carousel/index.js
+++ components/carousel/index.js
@@ -6,14 +6,18 @@ import "slick-carousel/slick/slick-theme.css";
 
 function MyCarousel({ children, className, ...config }) {
   const slider = useRef(null);
+  const [sliding, setSliding] = useState(false);
   const slide = (e) => {
+    if (sliding) return;
+    setSliding(true);
     const { deltaY } = e;
     deltaY / 200 > 0
       ? slider?.current.slickNext()
       : slider?.current.slickPrev();
+    setTimeout(() => setSliding(false), 800);
   };
   return (
-    <div onWheel={slide} onMouseOut={() => {}}>
+    <div onWheel={slide}>
       <Slider
         className={className}
         ref={slider}
